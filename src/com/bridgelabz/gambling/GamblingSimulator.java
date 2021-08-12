0e7f25dc5e5383fc78c0810b566f05f25fbbdc88
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-12 20:30:31 +0530

    Resigning of Player by winning or losing money

--- src/com/bridgelabz/gambling/GamblingSimulator.java
+++ src/com/bridgelabz/gambling/GamblingSimulator.java
@@ -5,14 +5,26 @@ public class GamblingSimulator {
 	public static final int bet = 1;
 	
 	public static void main(String[] args) {
-		 int rand = (int)Math.floor(Math.random()*10)%2;
+		int Stake = EveryDayStake;
 		
-		if (rand==bet) {
-			System.out.println("You Win");
+		while (Stake>50 && Stake<150) {
+			int rand = (int)Math.floor(Math.random()*10) % 2;
+			if (rand==bet) {
+				Stake = Stake + 1;
+				System.out.println("You Win");
+			}
+			else {
+				Stake = Stake - 1;
+				System.out.println("You Lose");
+			}	
+		}
+		if(Stake==50){
+			System.out.println("Player resigned due to loosing "+Stake);
 		}
 		else {
-			System.out.println("You Lose");
-		}		
+		System.out.println("Player resigned due to winning " +Stake);
+		}
+
 	}
 
 }
