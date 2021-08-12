3cbb9f6897ad07b9a26f2371a854d0cc365a7670
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-13 03:09:27 +0530

    Added months of playing

--- src/com/bridgelabz/gambling/GamblingSimulator.java
+++ src/com/bridgelabz/gambling/GamblingSimulator.java
@@ -6,24 +6,34 @@ public class GamblingSimulator {
 	
 	public static void main(String[] args) {
 		int Stake = EveryDayStake;
+		int month = 1;
 
-		
-		for(int i=0; i<=20; i++) {
+
+		while (month<=12) {
+			int day = 1;
+			System.out.println("Month " +month);
 			
-			int rand = (int)Math.floor(Math.random()*10) % 2;
-			if (rand==bet) {
-				Stake = Stake + 1;
+			while(day<=30) {
+				Stake = EveryDayStake;
+				
+				int rand = (int)Math.floor(Math.random()*10) % 2;
+				if (rand==bet) {
+					Stake = Stake + 1;		
+				}
+				else {
+					Stake = Stake - 1;					
+				}
+					
+				if(Stake<EveryDayStake){
+					System.out.println("Day "+day+" lost by "+(EveryDayStake - Stake)+" rupees ");				
+				}
+				else {
+					System.out.println("Day "+day+" won by " +(Stake - EveryDayStake)+" rupees ");				
+				}
+				day++;
 			}
-			else {
-				Stake = Stake - 1;
-			}
-			System.out.println(Stake);
-		}
-		if(Stake<EveryDayStake){
-			System.out.println("After 20 days Player lost by "+(EveryDayStake - Stake)+" rupees ");
-		}
-		else {
-			System.out.println("After 20 days Player won by " +(Stake - EveryDayStake)+" rupees ");
+			month++;
+			System.out.println();
 		}
 	}
 }
