b288aa1ec38c57b8b4a13cd54b708ed0e34bc867
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-12 12:22:55 +0530

    Win or Lose added

--- src/com/bridgelabz/gambling/GamblingSimulator.java
+++ src/com/bridgelabz/gambling/GamblingSimulator.java
@@ -5,8 +5,14 @@ public class GamblingSimulator {
 	public static final int bet = 1;
 	
 	public static void main(String[] args) {
-
+		 int rand = (int)Math.floor(Math.random()*10)%2;
 		
+		if (rand==bet) {
+			System.out.println("You Win");
+		}
+		else {
+			System.out.println("You Lose");
+		}		
 	}
 
 }
