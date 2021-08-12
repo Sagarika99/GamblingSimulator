e284ea7957c8522bd683ade3f6aa227916b61de8
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-12 12:03:00 +0530

    Stakes added

--- src/com/bridgelabz/gambling/GamblingSimulator.java
+++ src/com/bridgelabz/gambling/GamblingSimulator.java
@@ -1,9 +1,11 @@
 package com.bridgelabz.gambling;
 
 public class GamblingSimulator {
+	public static final int EveryDayStake = 100;
+	public static final int bet = 1;
+	
 	public static void main(String[] args) {
-		int EveryDayStake = 100;
-		int bet = 1;
+
 		
 	}
 
